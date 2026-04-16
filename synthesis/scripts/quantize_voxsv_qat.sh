ee59666878223eade11f07f786fe1ea7b7d1bb5f
안재우 <168390772+nyangmeong@users.noreply.github.com>
2026-04-17 02:53:11 +0900

    Add quantization script for VoxSV QAT

---
+++ synthesis/scripts/quantize_voxsv_qat.sh
@@ -0,0 +1,4 @@
+python quantize.py \
+    ~/Documents/Source/AI/ai8x-synthesis/trained/ai85-voxsv-qat.pth.tar \
+    ~/Documents/Source/AI/ai8x-synthesis/trained/ai85-voxsv-qat-q.pth.tar \
+    --device MAX78000
